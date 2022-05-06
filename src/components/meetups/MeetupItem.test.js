d78fbd4d0249b362254370ce5b6f2b0125b2df7e
roggc <roggc9@gmail.com>
2022-05-06 23:20:49 +0200

    unit test

--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -3,7 +3,7 @@ import { shallow, mount } from "enzyme";
 import MeetupItem from "./MeetupItem";
 import AllMeetupsPage from "../../pages/AllMeetupsPage";
 import AppProvider from "../../slices";
-import {act} from 'react-dom/test-utils'
+import { act } from "react-dom/test-utils";
 
 test("<MeetupItem/> renders without crashing", () => {
   const wrapper = shallow(
@@ -14,21 +14,23 @@ test("<MeetupItem/> renders without crashing", () => {
   expect(wrapper.exists()).toBe(true);
 });
 
-test("when on page 'ALL_MEETUP_PAGE', button 'add to favorites' must be present", async() => {
-  global.fetch=jest.fn()
-  global.fetch.mockResolvedValue({ json: () => ([{ }])})
+test("when on page 'ALL_MEETUP_PAGE', button 'add to favorites' must be present and when clicking it must disappear", async () => {
+  global.fetch = jest.fn();
+  global.fetch.mockResolvedValue({ json: () => [{}] });
   const component = mount(
     <AppProvider>
       <AllMeetupsPage>
-      <MeetupItem />
+        <MeetupItem />
       </AllMeetupsPage>
     </AppProvider>
   );
-    await act(async()=> Promise.resolve())
-    
-    component.update()
-      expect(component.find("button").length).toBe(1);
-      expect(component.find('button').at(0).text().trim()).toBe('Add to favorites') 
+  await act(async () => Promise.resolve());
+
+  component.update();
+  const buttons = component.find("button");
+  expect(buttons.length).toBe(1);
+  const button = buttons.at(0);
+  expect(button.text().trim()).toBe("Add to favorites");
+  button.simulate("click");
+  expect(component.find("button").length).toBe(0);
 });
-
-
